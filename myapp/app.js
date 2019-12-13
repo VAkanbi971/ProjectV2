ec5cda797065488ec343d27b49d9abc0c92fa4e0
Victor Akanbi <vakanbi971@west-mec.org>
2019-12-13 15:16:46 -0700

    Second Commit

--- myapp/app.js
+++ myapp/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var profileRouter = require('./routes/profile')
 
 var app = express();
 
@@ -23,6 +24,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/profile', profileRouter)
+
+// app.get('/', (req, res) => {
+//   res.redirect('/index');
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
