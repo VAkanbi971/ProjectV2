3a3b7c5490aa5006a7009837ac76d2d675237e3d
Victor Akanbi <vakanbi971@west-mec.org>
2019-12-17 15:26:44 -0700

    Fifth Commit
    Working on api's

--- myapp/app.js
+++ myapp/app.js
@@ -11,25 +11,7 @@ var app = express();
 
 var port = "3000";
 
-// lorem start
 
-const LoremIpsum = require("lorem-ipsum").LoremIpsum;
-
-const lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
-});
-
-lorem.generateWords(1);
-lorem.generateSentences(5);
-lorem.generateParagraphs(7);
-// lorem end
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
