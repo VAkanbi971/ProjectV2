09b6a1b367cc7aed7990cfd14b21dc7010d6cf42
Victor Akanbi <vakanbi971@west-mec.org>
2019-12-18 15:22:47 -0700

    Sixth Commit

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,28 @@
+
+const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+// configuration for lib
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
+});
+
+const word = lorem.generateWords(1);
+const sentence = lorem.generateSentences(5);
+const paragraph = lorem.generateParagraphs(7);
+
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Viktor'});
+  res.render('index', { title: word});
 });
 
 module.exports = router;
